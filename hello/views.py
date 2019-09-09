b4fbb5775a4d879a827a2e56eda9c8cfc6b75cdd
Akouvi <kouedjinl@gmail.com>
2019-09-08 22:38:45 -0400

    created a template

--- hello/views.py
+++ hello/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+# you create a view using functions
+
+
+def myView(request):
+    return HttpResponse("Hello, world!")
